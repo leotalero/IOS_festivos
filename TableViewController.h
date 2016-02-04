f9218cca5582609df9a2d522e0f3a1686d8a22ba
leotalero <leo.20waw@hotmail.com>
2016-02-03 20:43:52 -0500

    festivos app

---
+++ TableViewController.h
@@ -0,0 +1,14 @@
+//
+//  TableViewController.h
+//  festivos1
+//
+//  Created by Leonardo Talero on 1/1/16.
+//  Copyright © 2016 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+
+@end
---
+++ festivos1/DetailViewController.h
@@ -0,0 +1,14 @@
+//
+//  DetailViewController.h
+//  festivos1
+//
+//  Created by Leonardo Talero on 1/1/16.
+//  Copyright © 2016 unir. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UINavigationItem *navbar;
+
+@end
